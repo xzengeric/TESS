11bdfac2adff07ab755dbd8b1795243138fd6c4e
Eric Zeng <38875467+xzengeric@users.noreply.github.com>
2020-02-26 01:32:39 -0800

    done

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const employees = [];
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const team = [];
+let id = 0;
 
 init();
+
 function init() {
     console.log("Welcome to Template Engine - Employee Summary")
     buildTeam();
-}
+};
 
 function buildTeam() {
     inquirer
@@ -30,7 +29,7 @@ function buildTeam() {
                 choices: ["Manager", "Engineer", "Intern"]
             },
         ]).then(val => {
-            // If the user says yes to another game, play again, otherwise quit the game
+
             if (val.role === "Manager") {
                 this.role = val.role;
                 buildManager(role);
@@ -46,6 +45,7 @@ function buildTeam() {
         });
 };
 function askAgain() {
+    id = id + 1;
     inquirer
         .prompt([
             {
@@ -54,6 +54,7 @@ function askAgain() {
                 message: "More team member?"
             }
         ]).then(val => {
+
             // If the user says yes to another game, play again, otherwise quit the game
             if (val.choice) {
                 buildTeam();
@@ -73,11 +74,6 @@ function buildManager(role) {
         },
         {
             type: "input",
-            name: "id",
-            message: " Please enter your ID?"
-        },
-        {
-            type: "input",
             name: "email",
             message: " Please enter your email?"
         },
@@ -88,16 +84,14 @@ function buildManager(role) {
         }
     ]).then(val => {
         this.name = val.name;
-        this.id = val.id;
-        this.email=val.email;
+        this.id = id;
+        this.email = val.email;
         this.officeNumber = val.officeNumber;
-        employees.push({
-            'name': name,
-            'id': id,
-            'role': role,
-            'email':email,
-            'officeNumber': officeNumber
-        });
+
+        const manager = new Manager(name, id, email, officeNumber);
+
+
+        team.push(manager);
         askAgain();
 
     });
@@ -113,11 +107,6 @@ function buildEngineer(role) {
         },
         {
             type: "input",
-            name: "id",
-            message: " Please enter your ID?"
-        },
-        {
-            type: "input",
             name: "email",
             message: " Please enter your email?"
         },
@@ -128,16 +117,15 @@ function buildEngineer(role) {
         }
     ]).then(val => {
         this.name = val.name;
-        this.id = val.id;
-        this.officeNumber = val.officeNumber;
+        this.id = id;
+        this.email = val.email;
         this.github = val.github;
-        employees.push({
-            'name': name,
-            'id': id,
-            'role': role,
-            'github': github,
-            'email':email
-        });
+
+        const engineer = new Engineer(name, id, email, github);
+
+
+        team.push(engineer);
+
         askAgain();
 
     });
@@ -153,11 +141,6 @@ function buildIntern(role) {
         },
         {
             type: "input",
-            name: "id",
-            message: " Please enter your ID?"
-        },
-        {
-            type: "input",
             name: "email",
             message: " Please enter your email?"
         },
@@ -168,16 +151,14 @@ function buildIntern(role) {
         }
     ]).then(val => {
         this.name = val.name;
-        this.id = val.id;
-        this.school = val.school;
+        this.id = id;
         this.email = val.email;
-        employees.push({
-            'name': name,
-            'id': id,
-            'role': role,
-            'school': school,
-            'email':email
-        });
+        this.school = val.school;
+
+        const intern = new Intern(name, id, email, school);
+
+        team.push(intern);
+
         askAgain();
 
     });
@@ -185,35 +166,9 @@ function buildIntern(role) {
 
 
 function done() {
-    console.log(employees);
-    render(employees);
+
+    fs.writeFileSync(outputPath, render(team));
+    console.log("File is create to", outputPath);
+
 };
 
-
-
-
-
-
-
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
